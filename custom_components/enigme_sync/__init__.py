3d89c0a3479f32b3cf9db5a74f0ac13f358ab6a8
MrJulien38 <151173942+MrJulien38@users.noreply.github.com>
2026-04-03 10:48:18 +0200

    Update __init__.py

--- custom_components/enigme_sync/__init__.py
+++ custom_components/enigme_sync/__init__.py
@@ -1,17 +1,22 @@
+"""Enigme Sync integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
-from .services import async_register_services
+
 
 async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up from YAML."""
     return True
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up from config entry."""
     hass.data.setdefault(DOMAIN, {})
-    await async_register_services(hass)
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
     hass.data.pop(DOMAIN, None)
     return True
